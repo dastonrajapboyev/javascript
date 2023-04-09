7d8f4cc0cb3af38f823068f150bbbb851b5e72e7
Doston <1764rajabovv@gmail.com>
2023-04-09 00:42:36 -0700

    js basic operations, data types, comparison,logical operations if else,conditional ope., ternary ope.,nullish ope.

---
+++ index.js
@@ -0,0 +1,76 @@
+// console.log("hello javascriptjon");
+
+// let arr = ["doston", 27, { name: "ruslon", age: 26 }, "nodira"];
+
+// console.log(arr[1]);
+// =================
+
+// basic operators
+
+// let a = 11;
+// let b = "22";
+
+// console.log(-b + a);
+
+// let a = 11;
+// console.log(a++); // 11
+// console.log(a); // 12
+// console.log(++a); // 13
+// console.log(a); //13
+// console.log(a++); // 13
+// console.log(a++); //14
+
+// let a = 1000;
+// a /= 10;
+// console.log(a);
+
+//========================
+
+// comparision
+
+// let a = 10;
+// let b = 10;
+// console.log(a == b); // true
+// console.log(a !== b); // false
+
+//====================
+
+// logical operations
+
+// OR ||
+
+// let doston = true;
+// let ruslon = true;
+
+// console.log(doston === true || ruslon === true);
+// console.log(doston || ruslon); // bu ikkin chi qisqaroq varianti
+
+// -==========---------
+
+// && AND
+
+// let id = true;
+// let face = true;
+
+// console.log(id && face);
+
+// console.log(!(1 !=='0' && true && 'hey'));
+
+
+// if(true){
+  //    console.log('havo issiq');
+  // }
+  // if (false){
+    //   console.log('havo sovuq');
+    // }
+    // let temp = 1;
+
+    // temp < 0 ? console.log('havo sovuqqu') : console.log('havo yaxshi');
+
+    // nullish 
+
+    // console.log(false ?? 1);
+    // console.log(NaN ?? 1);
+    console.log(null ?? 1);
+    console.log(undefined ?? 1);
+

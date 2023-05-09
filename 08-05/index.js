377bdf407ef1d979808adff82d6ceebbad621c8a
Doston <1764rajabovv@gmail.com>
2023-05-09 00:45:15 -0700

    while, do while, function-decloration, function-expression, use function wia parametr

---
+++ 08-05/index.js
@@ -0,0 +1,50 @@
+// while, do while===================
+// let i =3
+// while(i){
+//   alert(i)
+//   i--
+// }
+
+// let i =11
+// while(i){
+//   alert('hello doston you are learning js well');
+//   i--
+// }
+
+// ============== do while
+// let i = 5;
+// do {
+//   console.log("hello from do while");
+//   i--;
+// } while (i);
+
+// ============================= functions
+
+//  function decloration-------
+// function getAvr(){
+//   console.log("hello daston bro");
+//   console.log("hello daston bro");
+//   console.log("hello daston bro");
+// you can call function declarotion wherever you want
+// }
+// getAvr()
+//  function decloration-------
+
+//  function expression-------
+// const getAvr =function(){
+//   console.log('hello this is function expression');
+//   // you need to know about func expression this it should be declare on top of file
+//   // because js engine read the file through single thread and it's easy to create this func
+//   // when it declare on top of file
+// }
+// getAvr()
+// ---------------------
+//  use function with parametr
+// const getAvr = function (name, person, age) {
+//   console.log(`hello this is ${name} and my name is ${person} I'm ${age}`);
+  // you need to know about func expression this it should be declare on top of file
+  // because js engine read the file through single thread and it's easy to create this func
+  // when it declare on top of file
+// };
+// getAvr("function expression", "doston",28);
+//  function expression-------

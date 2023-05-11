212e616e3b531d5de8f4f8672ce8d79ab5adfe52
Doston <1764rajabovv@gmail.com>
2023-05-11 00:29:37 -0700

    introduction object, access object with '.', '[]'

---
+++ 10-05/index.js
@@ -0,0 +1,21 @@
+//  object ==================================
+
+// let myInfo ={
+//   name:"Doston",
+//   age: 28,
+//   goal: "frontend developer",
+//   skills: "html, css, bootstrap, figma, RWD",
+//   currentlyGoal: "learning javascript"
+// }
+// console.log(myInfo);
+
+// --------------------
+
+// let myInfo ={
+//   name:"Doston",
+//   age: 28,
+//   goal: "frontend developer",
+//   skills: "html, css, bootstrap, figma, RWD",
+//   currentlyGoal: "learning javascript"
+// }
+// console.log(myInfo.name);

d84aceca0b894a9a34d68ea51827731d9e04f214
Doston <1764rajabovv@gmail.com>
2023-05-13 00:17:57 -0700

    access to object has done

--- 10-05/index.js
+++ 10-05/index.js
@@ -42,3 +42,29 @@
 //   0: 1
 // }
 // console.log(myInfo[0]);
+
+// how to call key which is inside of object with another variable name ?
+
+// let myInfo = {
+//   name: "Doston",
+//   age: 28,
+//   goal: "frontend developer",
+//   skills: "html, css, bootstrap, figma, RWD",
+//   currentlyGoal: "learning javascript",
+//   0: 1,
+// };
+
+// let keyName = "name";
+// console.log(myInfo['goal']);
+
+// let myInfo = {
+//   name: "Doston",
+//   age: 28,
+//   goal: "frontend developer",
+//   skills: "html, css, bootstrap, figma, RWD",
+//   currentlyGoal: "learning javascript",
+//   0: 1,
+// };
+
+// let name = prompt('what is your goal ?');
+// console.log(myInfo["goal"]);

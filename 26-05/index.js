db1dc66643cdec35e6cd937a6ed61db8d98e946f
Doston <1764rajabovv@gmail.com>
2023-05-28 07:34:38 -0700

    array methods

---
+++ 26-05/index.js
@@ -0,0 +1,58 @@
+// array
+// let ar = [1, 4, 232, "doston", function test() {}, null, {}, true];
+
+// console.log(ar);
+// console.log(ar[0]);console.log(ar[1]);
+// console.log(ar[2]);console.log(ar[3]);console.log(ar[4]);
+// console.log(ar[5]);console.log(ar[6]);console.log(ar[7]);
+// console.log(ar[ar.length-1]); // to get last element of array
+
+//============ at method
+
+// let ar = [1, 4, 232, "doston", function test() {}, null, {}, true];
+
+// console.log(ar.at(-1));
+// console.log(ar.at(-2));
+// console.log(ar.at(-3));
+//===============new array method
+
+// let ar = [1, 4, 232, "doston", function test() {}, null, {}, true];
+
+// let ar = new Array();
+// let arr = [];
+// console.log(ar);
+// console.log(arr);
+
+// how to assign data to array from outside================
+
+// let ar = [];
+// ar[2] = "doston";
+// console.log(ar);
+
+// how to change array's index data====================
+
+// let ar = ["doston",28]
+
+// ar[0]= "dostonbek"
+// console.log(ar);
+
+// add information between data inside array with join()========
+
+// let ar = ["doston", 28, { name: 'doston', age: 28 }];
+
+// console.log(ar.join("/"));
+
+// how to add 2 array to each other with concat method ================
+// let ar1 = [1, 2, 3, 4];
+// let ar2 = [5, 6, 7];
+
+// console.log(ar1.concat(ar2));
+
+// loops in array ================
+
+// let ar1 = ["doston", "ruslon", "mubina", "muslima","yasmina","fotima","mustafo"];
+// for (i=0; i < ar1.length; i++) {
+//   console.log(ar1[i]);
+// } // this way to loop is old way
+
+
